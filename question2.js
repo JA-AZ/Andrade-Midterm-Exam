888ee7576c3d8e0c84e02cbb1a7e228ec4a32a5b
unknown <YY206@ccs.local>
2025-03-10 05:26:47 -0700

    Q2 JavaScript Fundamentals

---
+++ question2.js
@@ -0,0 +1,11 @@
+function filterEvenNumbers(arr) {
+    return arr.filter(function(num) {
+      return num % 2 === 0;
+    });
+}
+
+//Testing our script
+const numbers = [1,2,3,4,5,6];
+const filteredNumbers = filterEvenNumbers(numbers);
+console.log(filteredNumbers);
+
